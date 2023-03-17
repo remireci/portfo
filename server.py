8823918bd6ef9dd263edab434ddd7bd9999a5f74
remireci <113353082+remireci@users.noreply.github.com>
2023-03-17 11:14:21 +0100

    prod2

--- server.py
+++ server.py
@@ -37,3 +37,6 @@ def submit_form():
             return 'did not save to database'
     else:
         return 'something went wrong, try again'
+
+if __name__ == "__main__":
+    app.run(ssl_context=('cert.pem', 'key.pem'))
